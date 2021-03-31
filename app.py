acfc22e508cfac8cd90d400e299f607b5a618ecd
a-46 <75574153+a-46@users.noreply.github.com>
2021-03-31 10:26:23 +0800

    end of 2021/3/30, trying to send a mail and failed

--- app.py
+++ app.py
@@ -1,17 +1,31 @@
 import os 
 from flask import Flask
-from flask-mail import Mail
+from flask_mail import Mail, Message
+
 
 app = Flask(__name__)
 
+
 app.config.update(
-    MAIL_SERVER=os.getrnv('MAIL_SERVER')
-    MAIL_PORT=465
-    MAIL_USE_TLS=True
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-    MAIL_DEFAULT_SENDER=('dezhao', os.getenv(MAIL_USERNAME))
+    MAIL_SERVER=os.getenv('MAIL_SERVER'),
+    MAIL_PORT=465,
+    MAIL_USE_SSL = True,
+    MAIL_USE_TLS = True,
+    MAIL_USERNAME = os.getenv('MAIL_USERNAME'),
+    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD'),
+    MAIL_DEFAULT_SENDER=('dezhao', os.getenv('MAIL_USERNAME')),
 
 )
 
 mail = Mail(app)
+'''
+def send_mail(subject,to,body):
+    message = Message(subject, recipients=[to], body=body)
+    mail.send(message)
+
+send_mail('hello','janall <janall@163.com>','1234567899')
+2112694290
+message= Message(subject='abc', recipients=['janall <2112694290@qq.com>'], body='1234666') 
+from flask_mail import Message
+from app import mail
+'''
